fecd919019722dc1ad1b1f60cb7c4ee549cb199b
filick <fuwangcosy@163.com>
2018-01-14 17:28:37 -0500

    add BottleneckGRURCNCell

--- pytorch/rcn.py
+++ pytorch/rcn.py
@@ -1,8 +1,8 @@
 import torch
-from torch.nn import Module, Conv2d
+import torch.nn as nn
 
 
-class GRURCNCellBase(Module):
+class GRURCNCellBase(nn.Module):
 
     def __init__(self, xz, hz, xr, hr, xh, rh):
         super(GRURCNCellBase, self).__init__()
@@ -21,7 +21,7 @@ class GRURCNCellBase(Module):
         return h
 
 
-class ConvGRURCNCell(Module):
+class ConvGRURCNCell(nn.Module):
     r"""GRU-RCN cell using the conventional covolution as the inner operation.
 
     The convolutions on the hidden states must keep the size unchanged, so 
@@ -56,17 +56,18 @@ class ConvGRURCNCell(Module):
                  x_kernel_size=None, x_stride=None, x_padding=None):
         super(ConvGRURCNCell, self).__init__()
 
-        hz = Conv2d(channels, channels, kernel_size, padding=(kernel_size - 1) // 2)
-        hr = Conv2d(channels, channels, kernel_size, padding=(kernel_size - 1) // 2)
-        rh = Conv2d(channels, channels, kernel_size, padding=(kernel_size - 1) // 2)
-
         x_channels = x_channels or channels
         x_kernel_size = x_kernel_size or kernel_size
         x_stride = x_stride or 1
         x_padding = x_padding or ((kernel_size - 1) // 2)
-        xz = Conv2d(x_channels, channels, x_kernel_size, x_stride, x_padding)
-        xr = Conv2d(x_channels, channels, x_kernel_size, x_stride, x_padding)
-        xh = Conv2d(x_channels, channels, x_kernel_size, x_stride, x_padding)
+
+        hz = nn.Conv2d(channels, channels, kernel_size, padding=(kernel_size - 1) // 2)
+        hr = nn.Conv2d(channels, channels, kernel_size, padding=(kernel_size - 1) // 2)
+        rh = nn.Conv2d(channels, channels, kernel_size, padding=(kernel_size - 1) // 2)
+
+        xz = nn.Conv2d(x_channels, channels, x_kernel_size, x_stride, x_padding)
+        xr = nn.Conv2d(x_channels, channels, x_kernel_size, x_stride, x_padding)
+        xh = nn.Conv2d(x_channels, channels, x_kernel_size, x_stride, x_padding)
 
         self._cell = GRURCNCellBase(xz, hz, xr, hr, xh, rh)
 
@@ -74,7 +75,89 @@ class ConvGRURCNCell(Module):
         return self._cell(x, hidden)
 
 
+class BottleneckGRURCNCell(nn.Module):
+    r"""GRU-RCN cell using a bottleneck block as the inner operation.
+
+    If the sizes of input and hidden state are not the same, users must specify 
+    the for channel number and stride for the input-to-hidden convolution, to 
+    make output has the same size as the hidden state.
+
+    Args:
+        channels: the number of channels of hidden states. If x_channels is None,
+            it means the channels of inputs and hidden states are the same.
+        x_channels, x_stride:
+            parameters of the inputs-to-hidden convolutions.
+            If None, inputs and hidden states have the same sizes, so the
+            convolutions on them have no difference.
+
+    Inputs: x (input), hidden
+        - **x** (batch, channel, height, width): tensor containing input features.
+        - **hidden** (batch, channel, height, width): tensor containing the hidden
+            state for the precious time instance.
+
+    Outputs: h
+        - **h**: (batch, hidden_size): tensor containing the next hidden state
+    """
+
+    def __init__(self, channels, x_channels=None, x_stride=None):
+        super(BottleneckGRURCNCell, self).__init__()
+
+        x_channels = x_channels or channels
+        x_stride = x_stride or 1
+
+        hz = Bottleneck(channels, channels)
+        hr = Bottleneck(channels, channels)
+        rh = Bottleneck(channels, channels)
+
+        xz = Bottleneck(x_channels, channels, stride=x_stride)
+        xr = Bottleneck(x_channels, channels, stride=x_stride)
+        xh = Bottleneck(x_channels, channels, stride=x_stride)
+
+        self._cell = GRURCNCellBase(xz, hz, xr, hr, xh, rh)
+
+    def forward(self, x, hidden):
+        return self._cell(x, hidden)
+
+
+class Bottleneck(nn.Module):
+
+    def __init__(self, in_channels, out_channels, expansion=4, stride=1):
+        super(Bottleneck, self).__init__()
+        planes = out_channels // 4
+        self.conv1 = nn.Conv2d(in_channels, planes, kernel_size=1)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=stride, padding=1)
+        self.bn2 = nn.BatchNorm2d(planes)
+        self.conv3 = nn.Conv2d(planes, out_channels, kernel_size=1)
+        self.relu = nn.ReLU()
+
+    def forward(self, x):
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+        out = self.relu(out)
+
+        out = self.conv3(out)
+        return out
+
+
 # test
 if __name__ == '__main__':
-    gru_rcn = ConvGRURCNCell(64, 7)
+    gru_rcn = BottleneckGRURCNCell(64)
     print(gru_rcn)
+
+    from torch.autograd import Variable
+    x = Variable(torch.rand(10, 64, 32, 32))
+    h = Variable(torch.rand(10, 64, 32, 32))
+    gru_rcn(x, h)
+
+    gru_rcn = BottleneckGRURCNCell(64, 32, 2)
+    print(gru_rcn)
+
+    from torch.autograd import Variable
+    x = Variable(torch.rand(10, 32, 64, 64))
+    h = Variable(torch.rand(10, 64, 32, 32))
+    gru_rcn(x, h)
